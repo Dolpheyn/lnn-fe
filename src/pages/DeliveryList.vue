986a42a48593103f03cfe5b66d255d15d08eab79
Dolpheyn <farissufyan99@gmail.com>
2021-07-13 13:51:59 +0800

    Add DeliveryList with route entry

---
+++ src/pages/DeliveryList.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="q-pt-xl q-px-xl q-mx-xl">
+    <!--Active Delivery-->
+
+    <!--Available Deliveries-->
+  </q-page>
+</template>
+
+<script lang="ts">
+import { Order } from 'components/models';
+import { OrderStatus, getAllOrderStatuses } from 'components/enums'
+import { defineComponent } from 'vue';
+
+const orders: Order[] = [
+]
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: {},
+  data() {
+    const orderList: Order[] = orders;
+    const orderStatuses: OrderStatus[] = getAllOrderStatuses();
+
+
+    return { orderList, orderStatuses };
+  },
+  methods: {
+  }
+});
+</script>
